a0f7f4ee7d81fe2f0260c0572c84f557eb9aeaea
Cyro <coolronitchs@gmail.com>
2024-02-19 17:07:15 +0530

    merge sort solved

---
+++ Recursion/9_MergeSort.cpp
@@ -0,0 +1,64 @@
+
+
+class MergeSort
+{
+
+   void merge(vector<int> &arr, int low, int mid, int high)
+   {
+      vector<int> temp;    // temporary array
+      int left = low;      // starting index of left half of arr
+      int right = mid + 1; // starting index of right half of arr
+
+      while (left <= mid && right <= high)
+      {
+         if (arr[left] <= arr[right])
+         {
+            temp.push_back(arr[left]);
+            left++;
+         }
+         else
+         {
+            temp.push_back(arr[right]);
+            right++;
+         }
+      }
+
+      // if elements on the left half are still left //
+      while (left <= mid)
+      {
+         temp.push_back(arr[left]);
+         left++;
+      }
+
+      // if elements on the right half are still left //
+
+      while (right <= high)
+      {
+         temp.push_back(arr[right]);
+         right++;
+      }
+
+      // transfering all elements from temp to arr //
+      for (int i = low; i <= high; ++i)
+      {
+         arr[i] = temp[i - low];
+      }
+   }
+
+   void mS(vector<int> &arr, int low, int high)
+   {
+      if (low >= high)
+         return;
+
+      int mid = (low + high) / 2;
+      mS(arr, low, mid);
+      mS(arr, mid + 1, high);
+
+      merge(arr, low, mid, high);
+   }
+
+   void mergeSort(vector<int> &arr, int n)
+   {
+      mS(arr, 0, n - 1);
+   }
+}
