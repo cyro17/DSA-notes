6a0d28284a5bbee2c2443484d4262925dda5c80d
Cyro <coolronitchs@gmail.com>
2024-02-19 19:55:04 +0530

    m-coloring solved

---
+++ Recursion/10_M-Coloring.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+private:
+   bool isSafe(int node, int color[], bool graph[101][101], int n, int col)
+   {
+      for (int k = 0; k < n; ++k)
+      {
+         if (k != node && graph[k][node] == 1 && color[k] == col)
+            return false;
+      }
+      return true;
+   }
+
+   bool solve(int node, int color[], int m, int n, bool graph[101][101])
+   {
+      if (node == n)
+         return true;
+
+      for (int i = 1; i <= m; ++i)
+      {
+         if (isSafe(node, color, graph, n, i))
+         {
+            color[node] = i;
+            if (solve(node + 1, color, m, n, graph))
+               return true;
+
+            // backtrack
+            color[node] = 0;
+         }
+      }
+      return false;
+   }
+
+public:
+   bool graphColoring(bool graph[101][101], int m, int n)
+   {
+      int color[n] = {0};
+      if (solve(0, color, m, n, graph))
+      {
+         return true;
+      }
+      return false;
+   }
+};
