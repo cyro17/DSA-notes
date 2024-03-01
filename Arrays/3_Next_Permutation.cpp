dff3329cef1c885a5a6cded884b93fec1c94a12f
Cyro <coolronitchs@gmail.com>
2024-03-01 19:56:18 +0530

    next permutation updated

--- Arrays/3_Next_Permutation.cpp
+++ Arrays/3_Next_Permutation.cpp
@@ -1,6 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/* M-1 : Recursive approach
+
+   1. find all possible permutation and store them in a ds.
+   2. search input in the ds and print the result next to it.
+
+   T.C => O (N! * N)
+   S.C => constant
+
+
+   M-2 : using unbuilt-fn
+            next_permutation(array);
+
+   M-3 : optimized approach:
+         T.C => O(3N)
+         S.C => constt.
+
+   1. Find break-point =>  (first index from the end where there is dip ( arr[i]< arr[i+1] )).
+   2. If no such break-point is found i.e the array is in decreasing order ,
+         hence the given array is in the last permutation => get the next permutation (sort the given array.)
+   3. If (break-point )
+         find the smallest no greater than the pivot in the right half .   // pivot = arr[break-point] //
+         swap with the pivot .
+         reverse the entire right half.
+  */
 class Solution
 {
 public:
