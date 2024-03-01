b604e9cc83f5587576898299bcad13d324f4a1ba
Cyro <coolronitchs@gmail.com>
2024-03-01 19:39:26 +0530

    next permutation solved

---
+++ Arrays/3_Next_Permutation.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+   void nextPermutation(vector<int> &arr)
+   {
+      int n = arr.size(), i, j;
+
+      for (i = n - 2; i >= 0; i--)
+      {
+         if (arr[i] < arr[i + 1])
+         {
+            break;
+         }
+      }
+
+      // Check if pivot is not found
+      if (i < 0)
+      {
+         reverse(arr.begin(), arr.end());
+      }
+
+      // if pivot is found
+      else
+      {
+
+         // Find for the successor of pivot in suffix
+         for (j = n - 1; j > i; j--)
+         {
+            if (arr[j] > arr[i])
+            {
+               break;
+            }
+         }
+
+         // Swap the pivot and successor
+         swap(arr[i], arr[j]);
+
+         // Minimise the suffix part
+         reverse(arr.begin() + i + 1, arr.end());
+      }
+   }
+};
