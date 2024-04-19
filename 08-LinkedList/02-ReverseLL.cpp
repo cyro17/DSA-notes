26959248aa4cc0a260e65745f70dc4b55c8ca544
Cyro <coolronitchs@gmail.com>
2024-04-19 17:41:30 +0530

    reverse LL and detect Cycle solved

--- 08-LinkedList/02-ReverseLL.cpp
+++ 08-LinkedList/02-ReverseLL.cpp
@@ -11,79 +11,30 @@ Time Complexity: O(2N)
 Space Complexity: O(N)
  */
 
-class Node
-{
-public:
-  // Data stored in the node
-  int data;
-
-  // Pointer to the next node in the list
-  Node *next;
-
-  // Constructor with both data and
-  // next node as parameters
-  Node(int data1, Node *next1)
-  {
-    data = data1;
-    next = next1;
-  }
-
-  // Constructor with only data as a
-  // parameter, sets next to nullptr
-  Node(int data1)
-  {
-    data = data1;
-    next = nullptr;
-  }
-};
-
-// Function to reverse the
-// linked list using a stack
 Node *reverseLinkedList(Node *head)
 {
-  // Create a temporary pointer
-  // to traverse the linked list
-  Node *temp = head;
 
-  // Create a stack to temporarily
-  // store the data values
+  Node *temp = head;
   stack<int> st;
 
-  // Step 1: Push the values of the
-  // linked list onto the stack
   while (temp != nullptr)
   {
-    // Push the current node's
-    // data onto the stack
     st.push(temp->data);
 
-    // Move to the next node
-    // in the linked list
     temp = temp->next;
   }
 
-  // Reset the temporary pointer to
-  // the head of the linked list
   temp = head;
 
-  // Step 2: Pop values from the stack
-  // and update the linked list
   while (temp != nullptr)
   {
-    // Set the current node's data to
-    // the value at the top of the stack
-    temp->data = st.top();
 
-    // Pop the top element from the stack
+    temp->data = st.top();
     st.pop();
 
-    // Move to the next node
-    // in the linked list
     temp = temp->next;
   }
 
-  // Return the new head of
-  // the reversed linked list
   return head;
 }
 
@@ -108,37 +59,18 @@ Space Complexity: O(1)
 
 Node *reverseLinkedList_opt1(Node *head)
 {
-  // Initialize'temp' at
-  // head of linked list
+
   Node *temp = head;
-
-  // Initialize pointer 'prev' to NULL,
-  // representing the previous node
   Node *prev = NULL;
-
-  // Traverse the list, continue till
-  // 'temp' reaches the end (NULL)
   while (temp != NULL)
   {
-    // Store the next node in
-    // 'front' to preserve the reference
+
     Node *front = temp->next;
 
-    // Reverse the direction of the
-    // current node's 'next' pointer
-    // to point to 'prev'
     temp->next = prev;
-
-    // Move 'prev' to the current
-    // node for the next iteration
     prev = temp;
-
-    // Move 'temp' to the 'front' node
-    // advancing the traversal
     temp = front;
   }
 
-  // Return the new head of
-  // the reversed linked list
   return prev;
 }
