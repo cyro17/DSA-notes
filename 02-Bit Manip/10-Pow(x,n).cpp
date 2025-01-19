6781e0748a7e2438d29ec11f242248e233a8c522
Ronit Raj <coolronitchs@gmail.com>
2025-01-19 15:05:49 +0530

    Update 10-Pow(x,n).cpp

--- 02-Bit Manip/10-Pow(x,n).cpp
+++ 02-Bit Manip/10-Pow(x,n).cpp
@@ -7,24 +7,23 @@
       TC = log(N)
 */
 
-class Solution_opt
-{
-   double solve(int x, int n)
-   {
-      if (n < 0)
-         x = 1 / x;
-      n = abs(n);
-      double pow = 1;
-
-      while (n != 0)
-      {
-         if (n & 1 != 0)
-         {
-            pow = pow * x;
-         }
-         x *= x;
-         n = n >> 1;
-      }
-      return pow;
-   }
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 0) return 1.0;
+        if(n == 1) return x;
+        if(n < 0) x = 1 / x;
+        n = abs(n);
+        
+        double pow = 1;
+        while(n !=0){
+            if(n & 1 != 0){
+                pow = pow * x;
+            }
+            x *= x;
+            n >>= 1;
+        }
+        return pow;
+        
+    }
 };
