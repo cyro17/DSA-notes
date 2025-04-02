1e76bfb798ae18ccf7822ef797f36defe954d270
Ronit Raj <coolronitchs@gmail.com>
2025-04-02 23:16:03 +0530

    Update 05_CombSumII.cpp

--- 03-Recursion/05_CombSumII.cpp
+++ 03-Recursion/05_CombSumII.cpp
@@ -36,14 +36,14 @@ public:
 		findComb(i+1, target-arr[i], arr, res, ds);
 		ds.pop_back();
 	}
-}
+    }
 
-vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
-	sort(arr.begin(), arr.end());
-	vector<vector<int>> res;
-	vector<int> ds;
-	findComb(0, target, arr, res, ds);
-	return res;
-}
+	vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
+		sort(arr.begin(), arr.end());
+		vector<vector<int>> res;
+		vector<int> ds;
+		findComb(0, target, arr, res, ds);
+		return res;
+	}
 // 
 };
