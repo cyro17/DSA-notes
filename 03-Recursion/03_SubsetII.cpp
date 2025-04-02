dfcb7f00cbe5e85f9ee782ab49560494ea735b16
Ronit Raj <coolronitchs@gmail.com>
2025-04-02 18:20:35 +0530

    Update 03_SubsetII.cpp

--- 03-Recursion/03_SubsetII.cpp
+++ 03-Recursion/03_SubsetII.cpp
@@ -19,10 +19,11 @@ Approach:
 This will help us in generating all possible combinations but does not take care of the duplicates.
 Hence we will use a set to store all the combinations that will discard the duplicates**.
 
-Time Complexity: O( 2^n *(k log (x) )). 2^n  for generating every subset and k* log( x)  
-   to insert every combination of average length k in a set of size x.
-   After this, we have to convert the set of combinations back into a list of list /vector of vectors 
-   which takes more time.
+   Time Complexity: O( 2^n *(k log (x) )). 
+   	   2^n  for generating every subset and k* log( x)  
+	   to insert every combination of average length k in a set of size x.
+	   After this, we have to convert the set of combinations back into a list of list /vector of vectors 
+	   which takes more time.
 
    Space Complexity:  O(2^n * k) to store every subset of average length k. Since we are initially using
    a set to store the answer another O(2^n *k) is also used. 
