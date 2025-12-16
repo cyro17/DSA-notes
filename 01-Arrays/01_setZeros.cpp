0a3086d60111d630e50eaaddbc50823ec44460c3
Ronit Raj <coolronitchs@gmail.com>
2025-12-16 22:26:36 +0530

    Optimize setZeroes function with flag approach
    Optimized the setZeroes function to use flags for marking rows and columns that need to be set to zero, improving space complexity.

--- 01-Arrays/01_setZeros.cpp
+++ 01-Arrays/01_setZeros.cpp
@@ -50,5 +50,87 @@ public:
    }
 };
 
+/*
+Optimized code: 
+      Here we took two flags which marks the if the first row or first col has any zero , if it does then set the flag as true;
+      Now traverse the matrix from (1, 1) and track if element is 0 then mark the relative first row and col = 0
+      Now traverse again from (1, 1) and if the relative first row or col is 0 then mark the element as 0;
+
+      finally check the flag if it is marked, then mark the respective row or col as 0;
+
+      T.C = O ( N * M)
+      S.C = O (1 )  -- space saved and optimized
+ 
+*/
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& arr) {
+        int n = arr.size(), m = arr[0].size();
+
+        // keep two flags first row and first col to keep check if any of it has zero 
+        // later on if it has then mark the entire first row or col = 0
+        bool firstRowZero = 0, firstColZero = 0;
+
+        // check the first col if it has zero 
+        for (int i = 0; i < n; ++i)
+        {
+            if(arr[i][0] == 0){
+                firstColZero = 1;
+                break;
+            }
+        }
+
+        // check the first row if has zero 
+        for (int j = 0; j < m; ++j)
+        {
+            if(arr[0][j] == 0){
+                firstRowZero = 1;
+                break;
+            }
+        }
+
+        // traverse the array apart from first row and first col 
+        // and check if zero is present then mark the respective first row and first col = 0
+        for (int i = 1; i < n; ++i)
+        {
+            for (int j = 1; j < m; ++j)
+            {
+                if(arr[i][j] == 0){
+                    arr[0][j] = 0;
+                    arr[i][0] = 0;
+                } 
+            }
+        }
+
+        // now traverse again and check if the respective first row and col = 0
+        for (int i = 1; i < n; ++i)
+        {
+            for (int j = 1; j < m; ++j)
+            {
+                if(arr[i][0] == 0 || arr[0][j] == 0){
+                    arr[i][j] = 0;
+                }
+            }
+        }
+
+        // check the firstrow and firstcol flag, 
+        // if it has zero then mark the entrie sequence as 0 
+
+        if(firstRowZero){
+            for (int j = 0; j < m; ++j)
+            {
+                arr[0][j] = 0;
+            }
+        }
+
+        if(firstColZero){
+            for (int i = 0; i < n; ++i)
+            {
+                arr[i][0] = 0;
+            }
+        }
+    }
+};
 
 
