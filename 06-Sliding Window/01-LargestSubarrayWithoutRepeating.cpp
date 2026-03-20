022824edf20912a3971590bdfe883e3ef62207d1
Ronit Raj <coolronitchs@gmail.com>
2026-03-20 16:02:35 +0530

    Update 01-LargestSubarrayWithoutRepeating.cpp

--- 06-Sliding Window/01-LargestSubarrayWithoutRepeating.cpp
+++ 06-Sliding Window/01-LargestSubarrayWithoutRepeating.cpp
@@ -31,3 +31,53 @@ public:
       return true;
     }
 };
+
+TC = O(2N) 
+    
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+         int n = s.length();
+    boolean[] hash = new boolean[256];
+
+    int l = 0, r = 0, max_len = 0, len = 0;
+
+    while (r < n){
+
+      while(hash[s.charAt(r)] ){
+        hash[s.charAt(l)]= false;
+        l++;
+      }
+
+      hash[s.charAt(r)] = true;
+      max_len = Math.max(max_len, r - l + 1);
+      r++;
+
+    }
+    return max_len;
+    }
+}
+
+
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int[] hash = new int[256];
+        Arrays.fill(hash,-1);
+
+        int l = 0, r = 0, max_len = 0, len = 0;
+
+        while (r < n){
+
+        if(hash[s.charAt(r)] >= l){
+            l = Math.max(hash[s.charAt(r)] + 1, l);
+        }
+
+        hash[s.charAt(r)] = r;
+        max_len = Math.max(max_len, r - l + 1);
+        r++;
+
+        }
+        return max_len;
+    }
+}
