1ddc4e7e132a81ec87ae9e4e39f871f9dea01674
Ronit Raj <coolronitchs@gmail.com>
2026-03-20 14:53:24 +0530

    Update 01-LargestSubarrayWithoutRepeating.cpp

--- 06-Sliding Window/01-LargestSubarrayWithoutRepeating.cpp
+++ 06-Sliding Window/01-LargestSubarrayWithoutRepeating.cpp
@@ -1,8 +1,33 @@
 #include <bits/stdc++.h>
+// TC -> O(N^3) , SC = O(1)
+
 using namespace std;
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        int max_ = 0;
 
-int main()
-{
 
-  return 0;
-}
+        for(int i = 0; i<n; i++){
+          for(int j = i; j<n; j++){
+            string sub = s.substr(i, j-i + 1);
+            if(check(sub)){
+              max_ = max(max_, (int)sub.length());
+            }
+          }
+        }
+        return max_;
+
+    }
+  private: 
+    bool check(string s){
+
+      set<char> set;
+      for(char c: s){
+        if(set.find(c) != set.end())return false;
+        set.insert(c);
+      }
+      return true;
+    }
+};
