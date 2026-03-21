1caac0eddf6371f36bba72d6b30d8797af9cf8e1
Ronit Raj <coolronitchs@gmail.com>
2026-03-21 18:15:12 +0530

    Refactor maxScore function using sliding window

--- 06-Sliding Window/04-MaxScoreCards.cpp
+++ 06-Sliding Window/04-MaxScoreCards.cpp
@@ -2,52 +2,28 @@
 using sliding window: TC = O(2*K)
  */
 
-class Solution
-{
-public:
-  int maxScore_bf(vector<int> &arr, int k)
-  {
-    int n = arr.size();
-    int max_sum = 0;
-    int left_sum = 0, right_sum;
+class Solution {
+    public int maxScore(int[] arr, int k) {
+        int n = arr.length;
+        int max = 0,  lsum = 0, rsum = 0; 
+        for(int i = 0; i < k; i++){
+            lsum += arr[i];
+        }
 
-    // store sun of first k elements from the left
-    for (int i = 0; i < k; i++)
-    {
-      left_sum += arr[i];
-      max_sum = max(max_sum, left_sum);
-    }
+        max = lsum;
+        int r = n-1, l = k-1;
+        while (l>=0){
 
-    //  initialize right index as last element of the array
-    right = n - 1;
-    for (int i = k - 1; i >= 0; i++)
-    {
-      left_sum -= arr[i];
-      right_sum += arr[right];
-      right--;
-      max_sum = max(max_sum, left_sum + right_sum);
-    }
+            lsum -= arr[l];
+            rsum += arr[r];
+        
+            max = Math.max(max, lsum + rsum);
 
-    return max_sum;
-  }
-  int maxScore(vector<int> &arr, int k)
-  {
-    int n = arr.size();
-    int lsum = 0, rsum = 0, max_sum = INT_MIN;
-    for (int i = 0; i < k; ++i)
-    {
-      lsum += arr[i];
-      max_sum = max(max_sum, lsum);
-    }
+            r--;
+            l--;
+        }
+        return max;
 
-    int right = n - 1;
-    for (int i = k - 1; i >= 0; --i)
-    {
-      lsum -= arr[i];
-      rsum += arr[right];
-      right--;
-      max_sum = max(max_sum, lsum + rsum);
     }
-    return max_sum;
-  }
-};
+}
+
